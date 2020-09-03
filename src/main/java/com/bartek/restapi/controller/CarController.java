baa5b5394bffcaf80927c02c8b3193fc57e87d4f
Bartosz Klocek <bartoszklocek1985@gmail.com>
2020-09-03 09:52:33 +0200

    Add method findById to controller

--- src/main/java/com/bartek/restapi/controller/CarController.java
+++ src/main/java/com/bartek/restapi/controller/CarController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,14 @@ public class CarController {
     @GetMapping
     public ResponseEntity<List<Car>> getAll() {
         return new ResponseEntity<>(carService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> findById(@PathVariable long id) {
+        if (carService.findCarByID(id).isPresent()) {
+
+            return new ResponseEntity<>(carService.findCarByID(id).get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
