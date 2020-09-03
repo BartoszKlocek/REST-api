d7386166748cfdb7d8c98b15c4cfed49a42a3163
Bartosz Klocek <bartoszklocek1985@gmail.com>
2020-09-03 09:45:08 +0200

    Add method getAll()

--- src/main/java/com/bartek/restapi/controller/CarController.java
+++ src/main/java/com/bartek/restapi/controller/CarController.java
@@ -1,9 +1,15 @@
 package com.bartek.restapi.controller;
 
+import com.bartek.restapi.model.Car;
 import com.bartek.restapi.service.CarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
@@ -14,5 +20,10 @@ public class CarController {
     @Autowired
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Car>> getAll() {
+        return new ResponseEntity<>(carService.findAll(), HttpStatus.OK);
     }
 }
