44a63f50f16c5dd8ee789e1ec34c47a796a209a6
Bartosz Klocek <bartoszklocek1985@gmail.com>
2020-09-03 09:41:53 +0200

    Add @RequestMapping(/cars)

--- src/main/java/com/bartek/restapi/controller/CarController.java
+++ src/main/java/com/bartek/restapi/controller/CarController.java
@@ -2,9 +2,11 @@ package com.bartek.restapi.controller;
 
 import com.bartek.restapi.service.CarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cars")
 public class CarController {
 
     private CarServiceImpl carService;
